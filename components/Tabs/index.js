d79f769a28a2a24316194f25c87956004b3043fa
Adan Rodriguez <arodriguez_11@live.com>
2020-02-23 19:49:50 -0600

    Finished Project

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,25 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((response) => {
+        // console.log(response.data.topics)
+        let tabs = response.data.topics
+        tabs.forEach((tab) => {
+            createTopics(tab);
+        })        
+    })
+
+function createTopics(data) {
+
+    let topicAnchor = document.querySelector('.topics')
+
+    let topicTab = document.createElement('div')
+    topicTab.classList.add('tab')
+    topicTab.textContent = data
+
+    topicAnchor.appendChild(topicTab)
+
+    return topicAnchor;
+} 
